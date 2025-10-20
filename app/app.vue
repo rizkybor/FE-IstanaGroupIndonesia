53128d2050bc3ff7f4ff8159018a63fb5bc221c8
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2025-10-20 12:21:23 +0700

    init-project
    scaffold, foldering

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-sky-500 to-indigo-600 text-white">
+    <h1 class="text-5xl font-extrabold mb-6 animate-bounce">
+      Tailwind Test Page
+    </h1>
+    <p class="text-lg bg-white/20 px-6 py-3 rounded-xl shadow-lg">
+      🎨 Jika kamu melihat warna gradasi biru dan teks melayang, Tailwind sudah aktif!
+    </p>
+    <button class="mt-6 px-6 py-3 bg-white text-sky-600 font-semibold rounded-lg shadow hover:bg-sky-100 transition">
+      Test Button
+    </button>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  vite: { plugins: [tailwindcss()] },
+  runtimeConfig: {
+    public: {
+      apiBase: 'https://fakestoreapi.com',
+    },
+  },
+  app: {
+    head: {
+      title: 'FETEST-IGI',
+      meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    },
+  },
+});
